ff2c30d6bd6b8980c76e225f82d3b0db22534271
marshmallow3210 <94796124+marshmallow3210@users.noreply.github.com>
2024-08-09 22:22:51 +0800

    reupload data in cmd

--- reupload.py
+++ reupload.py
@@ -14,10 +14,6 @@ connection = pymysql.connect(host='127.0.0.1',
                             password='66386638',
                             autocommit=True)
 
-# 場域設定
-databaseName = "fishDB"
-aquarium_id = "144" 
-food_name = "測試"
 
 ''' date format setting ''' 
 def utc8(utc, p):
@@ -146,67 +142,84 @@ def send_data(start_time):
 if __name__ == "__main__":
     cursor = connection.cursor()
 
+    # 場域設定
+    databaseName = input("請輸入 databaseName: ") # "fishDB"
+    aquarium_id = input("請輸入 aquarium_id: ") # "144" 
+    food_name = input("請輸入 food_name: ") # "測試"
+    query_date = input("請輸入 query_date (格式: YYYY-MM-DD): ")
     new_feeding_logs = []
-    file_path = '20240717_09.txt' # 檔案名稱要記得改
-    with open(file_path, 'r') as file:
-        for line in file:
-            columns = line.split(',')
-            # 檢查是否為 'off'
-            if columns[2].strip() == "'off'":
-                if new_feeding_logs:
-                    start_time = new_feeding_logs[0][1]
-                    end_time = new_feeding_logs[-1][1]
 
-                    use_time = end_time - start_time
-                    use_time = use_time.total_seconds() / 60
-                    use_time = round(use_time, 2)
+    sql = f"SELECT * FROM {databaseName}.ESP32 WHERE date = %s ORDER BY CONCAT(date, ' ', time) ASC;"
+    cursor.execute(sql, (query_date,))
+    rows = cursor.fetchall()
+    
+    for row in rows:
+        columns = list(row)
+        blower_state = columns[2].strip()
 
-                    # counting feeding_amount 
-                    weight = [float(log[0].split(',')[0]) for log in new_feeding_logs]
-                    feeding_amount = 0
-                    if weight:
-                        feeding_benchmark = weight[0]
-                        for i in range(1, len(weight)):
-                            if weight[i] <= weight[i-1] and weight[i] <= feeding_benchmark:
-                                feeding_amount += weight[i-1] - weight[i]
-                            else:
-                                feeding_benchmark = weight[i]
-                        print('feeding_benchmark:', feeding_benchmark)
-                        print('feeding_amount:', feeding_amount)
-                        left_amount = weight[-1]
-                        print('left_amount', left_amount)
-                    else:
-                        print("The weight list is empty.")
-                        left_amount = 0 
-                    
-                    food_id_map = {
-                        "測試": 39,         # fishDB
-                        "海洋牌": 40,       # ar2DB
-                        "漢神牌": 41,       # ar4DB
-                        "海洋飼料": 42      # ar3DB
-                    }
-                    food_id = food_id_map.get(food_name, 39)
+        # 轉換 timedelta 為 HH:MM:SS 格式
+        time_delta = columns[6]
+        total_seconds = int(time_delta.total_seconds())
+        hours, remainder = divmod(total_seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        time_str = f"{hours:02}:{minutes:02}:{seconds:02}"
 
-                    # insert to database
-                    sql = f"INSERT INTO {databaseName}.new_feeding_logs (journal_id, start_time, use_time, food_id, food_name, feeding_amount, left_amount, status, description) VALUES (0, '{str(start_time)}', {use_time}, {food_id}, '{str(food_name)}', {feeding_amount}, {left_amount}, 'good', '網路斷線重新上傳');"
-                    cursor.execute(sql)
+        # 組合日期和時間字串
+        timestamp_str = f"{columns[7]} {time_str}"
+        timestamp = datetime.datetime.strptime(timestamp_str, '%Y-%m-%d %H:%M:%S')
+        print(f"timestamp: {timestamp}, blower_state: {blower_state}")
+        
+        if blower_state == "off":
+            if new_feeding_logs != []:
+                start_time = new_feeding_logs[0][0]
+                end_time = new_feeding_logs[-1][0]
 
-                    # api integration, 午仔魚的場域不用
-                    send_data(str(start_time))
-                    print("send_data finished!\n")
+                use_time = end_time - start_time
+                use_time = use_time.total_seconds() / 60
+                use_time = round(use_time, 2)
 
-                    # reset new_feeding_logs
-                    new_feeding_logs=[]
+                # counting feeding_amount 
+                weight = [float(log[1]) for log in new_feeding_logs]
+                print(f"weight: {weight}")
+                feeding_amount = 0
+                if weight:
+                    feeding_benchmark = weight[0]
+                    for i in range(1, len(weight)):
+                        if weight[i] <= weight[i-1] and weight[i] <= feeding_benchmark:
+                            feeding_amount += weight[i-1] - weight[i]
+                        else:
+                            feeding_benchmark = weight[i]
+                    print('feeding_benchmark:', feeding_benchmark)
+                    print('feeding_amount:', feeding_amount)
+                    left_amount = weight[-1]
+                    print('left_amount', left_amount)
                 else:
-                    timestamp_str = columns[-1].strip()
-                    timestamp = datetime.datetime.strptime(timestamp_str, '%Y%m%d%H%M')
-                    print(f'blower_state: {columns[2].strip()}, time: {timestamp}')
+                    print("The weight list is empty.")
+                    left_amount = 0 
+                
+                food_id_map = {
+                    "測試": 39,         # fishDB
+                    "海洋牌": 40,       # ar2DB
+                    "漢神牌": 41,       # ar4DB
+                    "海洋飼料": 42      # ar3DB
+                }
+                food_id = food_id_map.get(food_name, 39)
+
+                # insert to database
+                sql = f"INSERT INTO {databaseName}.new_feeding_logs (journal_id, start_time, use_time, food_id, food_name, feeding_amount, left_amount, status, description) VALUES (0, '{str(start_time)}', {use_time}, {food_id}, '{str(food_name)}', {feeding_amount}, {left_amount}, 'good', '網路斷線重新上傳');"
+                cursor.execute(sql)
+
+                # api integration, 午仔魚的場域不用
+                send_data(str(start_time))
+                print("send_data finished!\n")
+
+                # reset new_feeding_logs
+                new_feeding_logs=[]
             else:
-                # if blower_state == 'on'
-                timestamp_str = columns[-1].strip()
-                timestamp = datetime.datetime.strptime(timestamp_str, '%Y%m%d%H%M')
-                print(f'blower_state: {columns[2].strip()}, time: {timestamp}')
-                new_feeding_logs.append((line.strip(), timestamp))
+                print(f'new_feeding_logs is empty')
+        else:
+            new_feeding_logs.append([timestamp, columns[0]])
+            print(new_feeding_logs)
             
     cursor.close()
     connection.close()
